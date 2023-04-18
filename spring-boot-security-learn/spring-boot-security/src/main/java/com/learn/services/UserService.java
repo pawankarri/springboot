9c0329f4155437271e715a5e32c79fb5e84b7a02
pawankarri <121856461+pawankarri@users.noreply.github.com>
2023-04-18 16:21:56 +0530

    Add files via upload

---
+++ spring-boot-security-learn/spring-boot-security/src/main/java/com/learn/services/UserService.java
@@ -0,0 +1,34 @@
+package com.learn.services;
+
+import com.learn.models.User;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Service
+public class UserService {
+    List<User> list=new ArrayList<>();
+
+    public UserService() {
+        list.add(new User("abc","abc","abc@gmail.com"));
+        list.add(new User("xyz","xyz","xyz@gmail.com"));
+    }
+    //get users
+    public List<User> getAllUsers()
+    {
+        return this.list;
+    }
+    //get single user
+    public User getUser(String username)
+    {
+        return this.list.stream().filter((user)->user.getUsername().equals(username)).findAny().orElse(null);
+    }
+    //add user
+    public User addUser(User user)
+    {
+        this.list.add(user);
+        return user;
+    }
+}
